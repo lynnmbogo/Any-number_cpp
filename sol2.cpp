0f873375629b5e2ae60e5216fdf3eae896597272
OOP-II Student <Mlynnmuthoni@gmail.com>
2016-04-22 13:51:28 +0300

    commit 2

---
+++ sol2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+  int n;
+  cin>>n;
+  for (; n-->0;)
+   {  
+     cout <<"hello, world!\n";
+ }
+ return 0;
+}
---
+++ sol8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+   int n;
+   cin>>n;
+   while(n-->0)
+    {
+      cout<<"Hello, World!\n";
+    }
+   return 0;
+  }
